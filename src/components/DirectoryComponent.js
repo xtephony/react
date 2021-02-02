395b6dcda6c67ccda19afa3063a20a40d2f15e64
Stephanie Johnson <sjohns34@hotmail.com>
2021-02-02 17:13:23 -0500

    updated with functions

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
-//importing built in components in reactstrap
+import React from 'react';
+/*importing built in components in reactstrap*/
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 
-// class components are objects declared as a class by { Directory />}
-class Directory extends Component {
-  
-// the render method
-    render() {
-         const directory = this.props.campsites.map(campsite => {
+function RenderDirectoryItem({campsite, onClick}) {
+    return(
+        <Card onClick={() => onClick(campsite.id)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>                        
+                </CardImgOverlay>                     
+        </Card>                   
+    );
+}
+
+/* class components are objects declared as a class by { Directory />} */
+function Directory(props) {
+    const directory = props.campsites.map(campsite => {
             return (
                 // this is the JSX 
                 <div key={campsite.id} className = "col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>                        
-                        </CardImgOverlay>                     
-                    </Card>                   
+                   <RenderDirectoryItem campsite={campsite} onClick={props.onClick} /> 
                 </div>
             );
         });
-// this is what's shown on the screen and is considered the main return
+
+
+/* this is what's shown on the screen and is considered the main return */
         return (
             <div className = "container">
                 <div className = "row">
@@ -31,6 +36,6 @@ class Directory extends Component {
             </div>
         );
     }
-}
+
 
 export default Directory;
