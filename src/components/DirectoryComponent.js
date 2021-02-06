b39ed0f5009c0af63ffeb2968cff3fbb73473e75
Stephanie Johnson <sjohns34@hotmail.com>
2021-02-06 16:14:05 -0500

    updated with breadcrumbs and aboutus component

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 function RenderDirectoryItem({campsite}) {
     return (
@@ -30,11 +30,21 @@ function Directory(props) {
 
 /* this is what's shown on the screen and is considered the main return */
         return (
-            <div className = "container">
-                <div className = "row">
-                    {directory}
+            <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
                 </div>
             </div>
+            <div className="row">
+                {directory}
+            </div>
+        </div>
         );
     }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -56,6 +57,7 @@ class Main extends Component {
                         <Route path='/home' component={HomePage} />
                         <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                         <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                        <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
                         <Route exact path='/contactus' component={Contact} />
                         <Redirect to='/home' />
                     </Switch>
