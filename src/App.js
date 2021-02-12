156eaefae0a1c34c54aabdd6e71020a77d18d4cc
Stephanie Johnson <sjohns34@hotmail.com>
2021-02-12 14:45:55 -0500

    redux added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import './App.css';
 
+const store = ConfigureStore();
 
 class App extends Component {
       //render method returns the directory component with a props to it
     // props name is campsites from the state object with the value CAMPSITES, an array passed into the directory component
     render() {
         return (
+            <Provider store={store}>
             <BrowserRouter>
                 <div className="App">
                     <Main />
                 </div>
             </BrowserRouter>
+        </Provider>
         );
     };
 }
